a073ad1fcbe8e401d7369e4e737897bd64eb6449
Fernando Reyes N <fersland@outlook.es>
2024-01-24 14:54:01 -0500

    bd

---
+++ script.sql
@@ -0,0 +1,169 @@
+
+CREATE DATABASE prueba
+USE [prueba]
+GO
+/****** Object:  Table [dbo].[cargos]    Script Date: 24/01/2024 14:52:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[cargos](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[codigo] [nvarchar](30) NOT NULL,
+	[nombre] [nvarchar](100) NOT NULL,
+	[activo] [bit] NOT NULL,
+	[idUsuario] [bigint] NOT NULL,
+	[created_at] [datetime] NULL,
+	[updated_at] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[departamentos]    Script Date: 24/01/2024 14:52:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[departamentos](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[codigo] [nvarchar](30) NOT NULL,
+	[nombre] [nvarchar](100) NOT NULL,
+	[activo] [bit] NOT NULL,
+	[idUsuario] [bigint] NOT NULL,
+	[created_at] [datetime] NULL,
+	[updated_at] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[failed_jobs]    Script Date: 24/01/2024 14:52:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[failed_jobs](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[uuid] [nvarchar](255) NOT NULL,
+	[connection] [nvarchar](max) NOT NULL,
+	[queue] [nvarchar](max) NOT NULL,
+	[payload] [nvarchar](max) NOT NULL,
+	[exception] [nvarchar](max) NOT NULL,
+	[failed_at] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[migrations]    Script Date: 24/01/2024 14:52:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[migrations](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[migration] [nvarchar](255) NOT NULL,
+	[batch] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[password_reset_tokens]    Script Date: 24/01/2024 14:52:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[password_reset_tokens](
+	[email] [nvarchar](255) NOT NULL,
+	[token] [nvarchar](255) NOT NULL,
+	[created_at] [datetime] NULL,
+ CONSTRAINT [password_reset_tokens_email_primary] PRIMARY KEY CLUSTERED 
+(
+	[email] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[personal_access_tokens]    Script Date: 24/01/2024 14:52:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[personal_access_tokens](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[tokenable_type] [nvarchar](255) NOT NULL,
+	[tokenable_id] [bigint] NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
+	[token] [nvarchar](64) NOT NULL,
+	[abilities] [nvarchar](max) NULL,
+	[last_used_at] [datetime] NULL,
+	[expires_at] [datetime] NULL,
+	[created_at] [datetime] NULL,
+	[updated_at] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[users]    Script Date: 24/01/2024 14:52:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[users](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
+	[email] [nvarchar](255) NOT NULL,
+	[email_verified_at] [datetime] NULL,
+	[password] [nvarchar](255) NOT NULL,
+	[remember_token] [nvarchar](100) NULL,
+	[created_at] [datetime] NULL,
+	[updated_at] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[usuarios]    Script Date: 24/01/2024 14:52:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[usuarios](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[usuario] [nvarchar](80) NOT NULL,
+	[primerNombre] [nvarchar](40) NOT NULL,
+	[segundoNombre] [nvarchar](40) NOT NULL,
+	[primerApellido] [nvarchar](40) NOT NULL,
+	[segundoApellido] [nvarchar](40) NOT NULL,
+	[idDepartamento] [bigint] NOT NULL,
+	[idCargo] [bigint] NOT NULL,
+	[created_at] [datetime] NULL,
+	[updated_at] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[failed_jobs] ADD  DEFAULT (getdate()) FOR [failed_at]
+GO
+ALTER TABLE [dbo].[usuarios]  WITH CHECK ADD  CONSTRAINT [usuarios_idcargo_foreign] FOREIGN KEY([idCargo])
+REFERENCES [dbo].[cargos] ([id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[usuarios] CHECK CONSTRAINT [usuarios_idcargo_foreign]
+GO
+ALTER TABLE [dbo].[usuarios]  WITH CHECK ADD  CONSTRAINT [usuarios_iddepartamento_foreign] FOREIGN KEY([idDepartamento])
+REFERENCES [dbo].[departamentos] ([id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[usuarios] CHECK CONSTRAINT [usuarios_iddepartamento_foreign]
+GO
