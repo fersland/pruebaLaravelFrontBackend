de3f0fec7be58852072ec8fb58e1aebd9d220df3
Fernando Reyes N <fersland@outlook.es>
2024-02-02 23:45:55 -0500

    BACKFRONT-
    De MSSQL a MySQL

--- BACKEND/prueba/config/database.php
+++ BACKEND/prueba/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlsrv'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -47,9 +47,9 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'port' => env('DB_PORT', '3308'),
+            'database' => env('DB_DATABASE', 'Lav_Administracion'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
